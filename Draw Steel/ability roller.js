4dca96cf2454f09e47a61d36943182590ab8bce6
jcheung9941 <75763997+jcheung9941@users.noreply.github.com>
2025-01-30 18:40:25 -0500

    merged chat events

--- Draw Steel/ability roller.js
+++ Draw Steel/ability roller.js
@@ -1,4 +1,4 @@
-//to add new ability, copy an existing ability and paste it in, be sure to also capture the }, at the end
+//to add new ability, copy an existing ability and paste it in, be sure to also capture the }, at the end..some
 //modify the name, putting it in quotes and ending with :{ example: [ "something something":{ ]
 //modify tier 1-3 and effect, be sure to enclose the text with " and end the line with , example: [ tier1:"something something something", ]
 const abilities = {
@@ -31,23 +31,23 @@ const dialog = { name: "error", modifier: 0, edge: { checked: 0, mod: 0, text: "
 await Dialog.prompt({
     title: 'Roll',
     content: `<form>
-    <div class="form-group"><label>Modifier:</label>
-        <div class="form-fields"><input name="mod" type="number" value="0" /></div>
-    </div>
-    <div class="form-group"><label>Edge/Bane:</label>
-      <select>
-        <option>Edge</option>
-        <option>Bane</option>
-      </select>
-      <input name="edge" type="checkbox" value=false />
-      <input name="edge" type="checkbox" value=false />
-    </div>
-    <div class="form-group"><label>Ability:</label>
-      <select name="ability">
-        ${Object.keys(abilities).map((element) => { return `<option>${element}</option>` })}
-      </select>
-    </div>
-</form>`,
+      <div class="form-group"><label>Modifier:</label>
+          <div class="form-fields"><input name="mod" type="number" value="0" /></div>
+      </div>
+      <div class="form-group"><label>Edge/Bane:</label>
+        <select>
+          <option>Edge</option>
+          <option>Bane</option>
+        </select>
+        <input name="edge" type="checkbox" value=false />
+        <input name="edge" type="checkbox" value=false />
+      </div>
+      <div class="form-group"><label>Ability:</label>
+        <select name="ability">
+          ${Object.keys(abilities).map((element) => { return `<option>${element}</option>` })}
+        </select>
+      </div>
+  </form>`,
     callback: html => {
         dialog.name = html.find('select[name=ability]')[0].value
         dialog.modifier = html.find('input[name=mod]').val()
@@ -61,28 +61,39 @@ await Dialog.prompt({
 })
 
 let roll = await new Roll(`2d10 + ${dialog.modifier}[mod] + ${dialog.edge.mod}[${dialog.edge.text}]`).roll({ async: true })
+let tier = 1
+let text = ""
+
+if (roll.total >= 12 && roll.total <= 16) {
+    tier = 2
+} else if (roll.total >= 17) {
+    tier = 3
+}
+
+if (dialog.edge.edge && dialog.edge.text === "Bane") {
+    tier = Math.max(tier - 1, 1)
+    text = `Bane Tier down in effect<hr/>`
+} else if (dialog.edge.edge) {
+    tier = Math.min(tier + 1, 3)
+    text = `Edge Tier up in effect<hr/>`
+} else if (Math.abs(dialog.edge.mod) === 2) {
+    text = `${dialog.edge.text} in effect<hr/>`
+}
+
+for (let i = 1; i < 4; i++) {
+    let line = `Tier${i}: ${abilities[dialog.name][`tier${i}`]} <br/>`
+    if (tier != i) {
+        text = text + line
+    } else {
+        text = `${text} <strong>${line}</strong>`
+    }
+}
+text = `${text} Effect: ${abilities[dialog.name].effect}`
+
 ChatMessage.create({
     roll: roll,
-    content: await roll.render(),
+    content: `
+    <div style="text-align:center"><br/><em><strong>${dialog.name}</strong></em> ${await roll.render()} <br/><strong>Result Tier${tier}</strong><br/>${abilities[dialog.name][`tier${tier}`]}</div><hr/>${text}`,
     sound: CONFIG.sounds.dice,
     type: CONST.CHAT_MESSAGE_TYPES.ROLL
 })
-
-const results = {
-    tier: 1,
-    text: `T1: ${abilities[dialog.name].tier1} <br/> T2: ${abilities[dialog.name].tier2} <br/> T3: ${abilities[dialog.name].tier3} <br/> Effect: ${abilities[dialog.name].effect}`
-}
-
-if (roll.total >= 12 && roll.total <= 16) {
-    results.tier = 2
-} else if (roll.total >= 17) {
-    results.tier = 3
-}
-if (dialog.edge.edge && dialog.edge.text === "Bane") {
-    results.tier = Math.max(results.tier - 1, 1)
-    results.text = `Bane Tier down in effect<hr/> ${results.text}`
-} else if (dialog.edge.edge) {
-    results.tier = Math.min(results.tier + 1, 3)
-    results.text = `Edge Tier up in effect<hr/> ${results.text}`
-}
-ChatMessage.create({ content: `<em>${dialog.name}</em><hr/><strong>Result Tier${results.tier}</strong><br/>${abilities[dialog.name][`tier${results.tier}`]}<hr/>${results.text}` })
